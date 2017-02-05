005b7d309cabcd363864dc99f939c6f680464a00
Gray Watson <gray.github@mailnull.com>
2017-02-05 09:29:36 -0500

    Some refactoring of code.  Added more div0 protection.

--- src/main/java/com/j256/simplemetrics/metric/ControlledMetricRatio.java
+++ src/main/java/com/j256/simplemetrics/metric/ControlledMetricRatio.java
@@ -104,46 +104,44 @@ public class ControlledMetricRatio extends BaseControlledMetric<NumeratorDenomin
 
 		@Override
 		public RatioValue makeAdjusted(NumeratorDenominator value) {
-			if (resetNext) {
-				double result = value.numerator / value.denominator;
+
+			if (resetNext || count == 0) {
+				double result = 0;
+				if (value.denominator != 0) {
+					result = value.numerator / value.denominator;
+				}
 				return new RatioValue(value.numerator, value.denominator, 1, result, result, false);
 			}
 
-			double newNumerator;
-			double newDenominator;
+			/*
+			 * This is a bit tricky. When adjusting the value, we must multiple the values together and not just add
+			 * them. If we add them then the last adjustment would get the most weight if it is a straight average.
+			 * 
+			 * So we basically do the cross product addition mechanism each time we adjust by another value and we will
+			 * divide by the count at the very end when the value is retrieved.
+			 * 
+			 * So if we are adding 1/2 + 1/3, we should get 5/6. 5/6 + 1/4 = 26/24. 26/24 + 1/2 = 76/48. Then the final
+			 * result is 76/(48*4) = 0.395833333.
+			 */
+			double newNumerator = this.numerator * value.denominator + value.numerator * this.denominator;
+			// denominator is the current denominator times the incoming one so 1/2 + 1/3 is some number of 6ths
+			double newDenominator = this.denominator * value.denominator;
+
+			double result = 0;
+			if (newDenominator != 0) {
+				result = newNumerator / newDenominator;
+			}
 			double min;
 			double max;
-			if (count == 0) {
-				newNumerator = value.numerator;
-				newDenominator = value.denominator;
-				min = newNumerator / newDenominator;
-				max = min;
+			if (result < this.min) {
+				min = result;
+				max = this.max;
+			} else if (result > this.max) {
+				min = this.min;
+				max = result;
 			} else {
-				/*
-				 * This is a bit tricky. When adjusting the value, we must multiple the values together and not add
-				 * them. If we add them then the last adjustment would get the most weight if it is a straight average.
-				 * 
-				 * So we basically do the cross product addition mechanism each time we adjust by another value and we
-				 * will divide by the count at the very end when the value is retrieved.
-				 * 
-				 * So if we are adding 1/2 + 1/3, we should get 5/6. 5/6 + 1/4 = 26/24. 26/24 + 1/2 = 76/48. Then the
-				 * final result is 76/(48*4) = 0.395833333.
-				 */
-				newNumerator = this.numerator * value.denominator + value.numerator * this.denominator;
-				// denominator is the current denominator times the incoming one so 1/2 + 1/3 is some number of 6ths
-				newDenominator = this.denominator * value.denominator;
-
-				double result = newNumerator / newDenominator;
-				if (result < this.min) {
-					min = result;
-					max = this.max;
-				} else if (result > this.max) {
-					min = this.min;
-					max = result;
-				} else {
-					min = this.min;
-					max = this.max;
-				}
+				min = this.min;
+				max = this.max;
 			}
 			return new RatioValue(newNumerator, newDenominator, count + 1, min, max, false);
 		}
